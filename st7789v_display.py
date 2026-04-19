7e2553ecd81e64d89cb7f9c481fcb5fd982f0593
SAIRPi Project <sairpiproject@gmail.com>
2026-04-19 11:28:08 +0100

    Update st7789v_display.py
    Added MIT license back to header

--- st7789v_display.py
+++ st7789v_display.py
@@ -35,6 +35,34 @@
 #   python3 /root/.tft/st7789v_display.py /root/.tft/jaffaworks_logo.png
 #
 ###
+#
+# LICENCE:
+# This script is released under the MIT License.
+# You are free to use, copy, modify, merge, publish, distribute, sublicense, 
+# and/or sell copies of this software, subject to the terms of the MIT License.
+# If you reuse this work, retain the original copyright notice and license text.
+#
+# Copyright (c) 2026 sairpi.penthux.net
+#
+# Permission is hereby granted, free of charge, to any person obtaining a 
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation 
+# the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+# and/or sell copies of the Software, and to permit persons to whom the 
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in 
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+#
+###
 
 
 import sys
